01f03f475cb4964ac3b1823fed4eb8ff9d442164
adminho <couragor@gmail.com>
2023-12-24 09:41:09 +0700

    no message

---
+++ soapservice/src/main/java/com/example/soapservice/SoapserviceApplication.java
@@ -0,0 +1,16 @@
+package com.example.soapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Cite: https://spring.io/guides/gs/producing-web-service/
+//Cite: https://github.com/spring-guides/gs-producing-web-service/tree/main/complete/src
+
+@SpringBootApplication
+public class SoapserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapserviceApplication.class, args);
+	}
+
+}
---
+++ soapservice/src/test/java/com/example/soapservice/SoapserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
