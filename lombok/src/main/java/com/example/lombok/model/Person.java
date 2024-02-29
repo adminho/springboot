d9fc9c8a91415a7fdc27871abbd48921fe158909
adminho <couragor@gmail.com>
2024-02-29 11:55:38 +0700

    Update Person.java

--- lombok/src/main/java/com/example/lombok/model/Person.java
+++ lombok/src/main/java/com/example/lombok/model/Person.java
@@ -1,7 +1,10 @@
 package com.example.lombok.model;
 
 import lombok.Data;
-
+// การใช้ lombok 
+// 1) ดาวน์โหลดไฟล์ lombok.jar
+// 2) ดับเบิลคลิก lombok.jar -> เลือกไฟล์ eclipse.exe, SpringToolSuite4.exe ที่จะปล้๊กอิน lombok เข้าไป
+// ปัญหาที่เคยเจอ สร้างโฟลเดอร์ Workspace ไว้ที่โฟลเดอร์หนึ่ง แต่ import โฟลเดอร์โปรเจค lombok อีกโฟลเดอร์กัน ก็จะใช้งาน lombok ไม่ได้
 @Data
 public class Person {
 	private String firstname;
